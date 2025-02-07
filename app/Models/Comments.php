bf43924417512711d10ecfb221ce8554f10d8e34
dhilip004 <dhilip0040@outlook.com>
2025-02-07 17:48:15 +0530

    updqate the code

--- app/Models/Comments.php
+++ app/Models/Comments.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Model;
+//use Illuminate\Database\Eloquent\Model;
 use App\Models\Replies;
 
 
@@ -28,7 +28,7 @@ class Comments extends Model
 }
 
 
-    public function Replies(){
+     function Replies(){
 
         return $this->hasMany('App\Models\Reply');
     }
